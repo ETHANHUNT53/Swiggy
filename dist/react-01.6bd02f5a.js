02cd3a7572099759d97daaf0a5e4fdb58405deab
shaurya <shaurya_jaiswal@epam.com>
2025-05-28 12:32:27 +0530

    Remove .parcel-cache and dist from repo and add to .gitignore

--- dist/react-01.6bd02f5a.js
+++
@@ -1,5 +0,0 @@
-const heading = React.createElement("h1", {}, "Hello World");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-
-//# sourceMappingURL=react-01.6bd02f5a.js.map
